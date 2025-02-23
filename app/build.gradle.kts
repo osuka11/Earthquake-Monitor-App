bb4d0ec353667254ae4c9f7d97df361b097e51be
Oscar Rincon <orincon0@ucol.mx>
2025-02-22 22:35:05 -0700

    Firts Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+import com.android.build.gradle.internal.utils.isKspPluginApplied
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp.devtools)
+}
+
+android {
+    namespace = "com.example.earthquake_app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.earthquake_app"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures{
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.recyclerView)
+    implementation(libs.androidx.lifecycle.ktx)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.ksp.room)
+    implementation(libs.retrofit)
+    implementation(libs.moshi)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
